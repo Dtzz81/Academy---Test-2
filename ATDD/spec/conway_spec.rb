17f48738e55d9a9892d48c59ce4081d25c1154f7
dtzz81 <denisa.turkova@madetech.com>
2022-11-01 09:33:30 +0000

    after second video

--- ATDD/spec/conway_spec.rb
+++ ATDD/spec/conway_spec.rb
@@ -1,5 +1,38 @@
 describe "does the test run?" do
-    it "it should come as an error" do
-        expect(true).to be false
+    it "it should come as an error if to be false" do
+        expect(true).to be true
     end
+end
+
+describe "see the next generation in GoL Game of Life" do
+    it "stay same for an empty grid" do
+        empty_grid = [
+            [ :empty, :empty, :empty],
+            [ :empty, :empty, :empty],
+            [ :empty, :empty, :empty]]
+        expect(see_the_next_generation(empty_grid)).to eq(empty_grid)    
+    end
+
+    it "returns an empty grid given one alive cell in the centre" do
+        one_cell_grid = [ 
+        [ :empty, :empty, :empty],
+        [ :empty, :alive, :empty],
+        [ :empty, :empty, :empty]
+        ]
+
+        empty_grid = [
+            [ :empty, :empty, :empty],
+            [ :empty, :empty, :empty],
+            [ :empty, :empty, :empty]
+        ]
+
+        expect(see_the_next_generation(one_cell_grid)).to eq(empty_grid) 
+    end
+end
+
+def see_the_next_generation(generation)
+    [
+        [ :empty, :empty, :empty],
+        [ :empty, :empty, :empty],
+        [ :empty, :empty, :empty]]
 end
